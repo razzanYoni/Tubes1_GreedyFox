6daa2d87d19911a90bc0561ad85720fbbf52bb1a
4kmalm <16521070@mahasiswa.itb.ac.id>
2023-02-17 23:05:48 +0700

    Fariming

--- src/main/java/Services/FarmingMode.java
+++ src/main/java/Services/FarmingMode.java
@@ -11,6 +11,7 @@ import java.util.stream.*;
 
 public class FarmingMode {
     // Atribut
+    // private Double afterburner_food_threshold;
     private Data gameDataFood;
     private GameObject gfbot;
     // private GameObject foodObj;
@@ -22,11 +23,12 @@ public class FarmingMode {
         this.gameDataFood = gameDataFood;
         this.gfbot = gfbot;
         this.gfAction = gfAction;
-        setSuperFoodThreshold(gfbot);
+        // setSuperFoodThreshold(gfbot);
     }
 
     // Copy Constructor
     public FarmingMode(FarmingMode fm) {
+        // this.afterburner_food_threshold = fm.afterburner_food_threshold;
         this.gameDataFood = fm.gameDataFood;
         this.gfbot = fm.gfbot;
         // this.foodObj = fm.foodObj;
@@ -34,17 +36,21 @@ public class FarmingMode {
     }
 
     // Membandingkan SuperFood dengan regular food lalu 
+    // public void setSuperFoodThreshold(GameObject obj) {
+    //     afterburner_food_threshold = (Double) (obj.getSize() * 0.02);  // Pake Radius Kah??
+    // }
+
     public boolean isBurnerNeeded() {
         if (gameDataFood.getnSuperFoodObject() <= 0) {
             return false;
         } else {
             var rf = gameDataFood.getFoodObjectDistance().get(0);
             var sf = gameDataFood.getSuperFoodObjectDistance().get(0);
-            /* superfood sama isburnerneeded */
+            /* superfood sama isburnerneeded*/
             return (sf < rf);
         }
     }
-
+    
     public void resolveFarmingFoodAction() {
         gfAction.action = PlayerActions.FORWARD;
         gfAction.setHeading(Statistic.getHeadingBetween(gfbot ,isBurnerNeeded() ? gameDataFood.getSuperFoodObject().get(0) : gameDataFood.getFoodObject().get(0)));
